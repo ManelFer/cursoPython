8782ab53a0426d3cf8daf48ca4b5d6a42e556df3
manoel ferreira <95552207+ManelFer@users.noreply.github.com>
2025-04-09 19:40:45 -0300

    que caos

--- flask_project/src/app.py
+++ flask_project/src/app.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'diobank.sqlite'),
     )
 
     if test_config is None:
@@ -23,6 +23,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    from . import db
+    db.init_app(app)
 
     # a simple page that says hello
     @app.route('/hello')
---
+++ flask_project/src/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY ,
+    "user" TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY ,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY ("author_id") REFERENCES "user" (id)
+);
