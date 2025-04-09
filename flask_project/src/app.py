686bc5b92721349c68f21a1a2400f99f100c0526
manoel ferreira <95552207+ManelFer@users.noreply.github.com>
2025-04-09 20:25:04 -0300

    deu bom

--- flask_project/src/app.py
+++ flask_project/src/app.py
@@ -1,13 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-
+import click
 class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(model_class=Base)
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
 
 
 def create_app(test_config=None):
@@ -15,7 +23,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE='diobank.sqlite',
+        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
     )
 
     if test_config is None:
@@ -26,14 +34,15 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-
-    from . import db
+    app.cli.add_command(init_db_command)
     db.init_app(app)
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    
 
     return app
